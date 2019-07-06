b826520335ddb03507c9e98d7208cdab4de77652
andersonlimahw <andersonlimahw@gmail.com>
2019-07-06 10:24:46 -0300

    [FEAT] - ValueObjects

--- XGame.Domain/Entities/Jogador.cs
+++ XGame.Domain/Entities/Jogador.cs
@@ -1,15 +1,16 @@
-﻿namespace XGame.Domain.Entities
+﻿using XGame.Domain.Enum;
+using XGame.Domain.ValueObjects;
+
+namespace XGame.Domain.Entities
 {
     public class Jogador
     {
-        public string PrimeiroNome { get; set; }
 
-        public string UltimoNome { get; set; }
-
-        public string Email { get; set; }
+        public Nome Nome { get; set; }
+        public Email Email { get; set; }
 
         public string Senha { get; set; }
 
-        public int Status { get; set; }
+        public EnumSituacaoJogador Status { get; set; }
     }
 }
---
+++ XGame.Domain/Enum/EnumSituacaoJogador.cs
@@ -0,0 +1,9 @@
+﻿namespace XGame.Domain.Enum
+{
+    public enum EnumSituacaoJogador
+    {
+        EmAndamento = 0,
+        Ativo = 1,
+        Bloqueado = 2 
+    }
+}
